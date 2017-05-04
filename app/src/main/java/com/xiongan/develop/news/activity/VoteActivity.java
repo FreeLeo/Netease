8aba9a58d1a690680ba16952be77ea1c57b2b924
lizhen <lizhen@brotsoft.com>
2017-05-04 16:49:24 +0800

    add 评论

--- app/src/main/java/com/xiongan/develop/news/activity/VoteActivity.java
+++ app/src/main/java/com/xiongan/develop/news/activity/VoteActivity.java
@@ -1,13 +1,35 @@
 package com.xiongan.develop.news.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+import com.xiongan.develop.news.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class VoteActivity extends AppCompatActivity {
+
+    @BindView(R.id.back_iv)
+    ImageView backIv;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.swiperefreshlayout)
+    SwipeRefreshLayout swiperefreshlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.back_iv)
+    public void onViewClicked() {
+        finish();
     }
 }
