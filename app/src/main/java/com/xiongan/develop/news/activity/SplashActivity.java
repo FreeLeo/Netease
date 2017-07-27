dc12c9ee68cd19a72db1f01ff667808daaa5ec80
lizhen <lizhen@brotsoft.com>
2017-07-27 19:02:41 +0800

    闪屏

---
+++ app/src/main/java/com/xiongan/develop/news/activity/SplashActivity.java
@@ -0,0 +1,129 @@
+package com.xiongan.develop.news.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
+import com.xiongan.develop.news.R;
+
+import rx.Subscriber;
+
+/**
+ * Created by apple on 16/4/17.
+ */
+public class SplashActivity extends Activity {
+    private View rootView;
+    private RxPermissions rxPermissions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        GoogleAnalytics.sendConversions("App_Launch");
+        //透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        rxPermissions = new RxPermissions(this);
+        rxPermissions.setLogging(true);
+        rootView = View.inflate(this, R.layout.fragment_splash, null);
+        setContentView(rootView);
+        if (Build.VERSION.SDK_INT >= 23) {
+            CheckPermission();
+        } else {
+            initStartAnimation();
+        }
+    }
+
+//    private void setLanguage() {
+//        Configuration configuration = new Configuration();
+////        Configuration config = getResources().getConfiguration();
+//        DisplayMetrics dm = getResources().getDisplayMetrics();
+//        int selectId = SharePreferenceUtil.getInstance(this).getLanguage();
+//        switch (selectId) {
+//            case 0://简体中文
+//                configuration.locale = Locale.SIMPLIFIED_CHINESE;
+//                break;
+//            case 1://英语
+//                configuration.locale = Locale.ENGLISH;
+//                break;
+//            default:
+//                break;
+//        }
+//        getResources().updateConfiguration(configuration, dm);
+//    }
+
+    private void CheckPermission() {
+        rxPermissions.request(Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.SYSTEM_ALERT_WINDOW,
+                Manifest.permission.CAMERA)
+                .subscribe(new Subscriber<Boolean>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.e("RxPermissions: Complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Loger.e("RxPermissions: Error: " + e.toString());
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        Loger.e("RxPermissions: Next: is?: " + aBoolean);
+                        if (aBoolean) {
+                            FDSdk.getInstance().init(SplashActivity.this);
+                        } else {
+                            Loger.e("Permission denied, can't enable the camera ");
+                        }
+                        initStartAnimation();
+                    }
+                });
+    }
+
+    private void initStartAnimation() {
+        // 渐变展示启动屏
+        AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
+        aa.setDuration(1500);
+        rootView.startAnimation(aa);
+        aa.setAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                gotoNext();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+        });
+    }
+
+    // 执行跳转
+    private void gotoNext() {
+        SharePreferenceUtil.getInstance(SplashActivity.this).setJump("1");
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        SplashActivity.this.finish();
+    }
+}
